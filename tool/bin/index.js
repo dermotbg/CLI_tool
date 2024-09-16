895a9b55219958ee30891fd69b8118d282e6233b
dermotbg <dermotbateman@gmail.com>
2024-09-16 07:20:12 +0300

    8/14 -> requrie busted

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,22 +1,36 @@
 #!/usr/bin/env node
-const arg = require('arg');
+// const arg = require('arg');
+// const chalk = require('chalk');
+
+import chalk from "chalk";
+import arg from "arg";
+import { packageUpSync } from 'package-up';
+
 
 try {
   const args = arg({
     '--start': Boolean,
     '--build': Boolean
-  })
+  });
   
   if(args['--start']){
-    console.log('starting the app');
+    const pkgPath = packageUpSync({ cwd: process.cwd() });
+    const pkg = require(pkgPath);
+    if (pkg.tool){
+      console.log('Found Configuration', pkg.tool)
+    } else {
+      console.log(chalk.yellow("Could not find configuration, using default"));
+    }
+
+    console.log(chalk.bgCyanBright('starting the app'));
   };
 } catch (error) {
-  console.log(error.message);
+  console.log(chalk.yellow(error.message));
   usage();
 }
 
 function usage() {
-  console.log(`tool [CMD]
-    --start\tStarts the app
-    --build \tBuilds the app`);
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+    ${chalk.greenBright('--start')}\tStarts the app
+    ${chalk.greenBright('--build')}\tBuilds the app`);
 }
